60bb83ed5e25a7894267f506b7aa9dcb9b7abb4b
Shayan <wil1i@pop-os.localdomain>
2023-07-25 15:37:00 +0330

    Key binds blocking disabled

--- public/js/main.js
+++ public/js/main.js
@@ -73,15 +73,16 @@ function ctrlShiftKey(e, keyCode) {
   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
 }
 
-document.onkeydown = (e) => {
-  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-  if (
-    event.keyCode === 123 ||
-    ctrlShiftKey(e, "I") ||
-    ctrlShiftKey(e, "J") ||
-    ctrlShiftKey(e, "C") ||
-    (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-  )
-    window.location.href = "eror.php";
-  return false;
-};
+// TODO : fix this
+// document.onkeydown = (e) => {
+//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+//   if (
+//     event.keyCode === 123 ||
+//     ctrlShiftKey(e, "I") ||
+//     ctrlShiftKey(e, "J") ||
+//     ctrlShiftKey(e, "C") ||
+//     (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+//   )
+//     window.location.href = "eror.php";
+//   return false;
+// };
