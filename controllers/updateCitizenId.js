4daf44de856b651b9c5e5da072263fbf3facf037
Shayan <wil1i@pop-os.localdomain>
2023-07-28 11:40:42 +0330

    UpdateCitizenId API updated

--- controllers/updateCitizenId.js
+++ controllers/updateCitizenId.js
@@ -1,13 +1,17 @@
 const User = require("../models/User")
 
 const post = async (req, res) => {
-    if (!req.body.citizenId || !req.body.username) return res.json({ ok: false })
+    if (!req.body.citizenId || !req.body.username || !req.body.code) return res.json({ ok: false })
 
     const isUserAvailable = await User.findOne({ where: { username: req.body.username } })
 
-    if (!isUserAvailable) return res.json({ ok: false })
+    if (
+        !isUserAvailable
+        || isUserAvailable.code != req.body.code
+        || isUserAvailable.verified
+    ) return res.json({ ok: false })
 
-    await isUserAvailable.update({ citizenId: req.body.citizenId })
+    await isUserAvailable.update({ citizenId: req.body.citizenId, code: null, verified: true })
 }
 
 module.exports = {
