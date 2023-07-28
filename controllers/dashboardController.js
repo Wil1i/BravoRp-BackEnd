ab390491ecbd6ef51911cd35eefa3bf1f3b7f4ae
Shayan <wil1i@pop-os.localdomain>
2023-07-28 13:49:51 +0330

    Dashboard fixed

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -3,12 +3,12 @@ const axios = require("axios")
 
 const get = async (req, res) => {
     let findPlayer, players;
-    // try {
-    //     players = await axios.get("http://127.0.0.1:30120/players.json") || { data: [] }
-    //     if (req.user.citizenId) {
-    //         findPlayer = await ServerPlayer.findOne({ where: { citizenId: req.user.citizenId }, include: ['permissions'] })
-    //     }
-    // } catch (error) { }
+    try {
+        players = await axios.get("http://127.0.0.1:30120/players.json") || { data: [] }
+        if (req.user.citizenId) {
+            findPlayer = await ServerPlayer.findOne({ where: { citizenId: req.user.citizenId }, include: ['permissions'] })
+        }
+    } catch (error) { }
 
     res.render("dashboardPage", {
         user: req.user,
